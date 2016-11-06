3814630b47a8c1c475fc140df2f60296a4381c6c
Mauricio Díaz Orlich <github@madd0.com>
2016-11-06 13:43:45 +0100

    Create solution and projects

---
+++ test/ChamaeleoIoT.Tests/Framework/BaseTestFixture{TSubject,TTestEnvironment}.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ChamaeleoIoT.Tests
+{
+    public abstract class BaseTestFixture<TSubject, TTestEnvironment>
+        where TTestEnvironment : TestEnvironment<TSubject>
+    {
+        public virtual TTestEnvironment GetEnvironment()
+        {
+            TTestEnvironment env = Activator.CreateInstance<TTestEnvironment>();
+
+            env.SetupEnvironment();
+
+            return env;
+        }
+
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+            this.OnTestFixtureSetUp();
+        }
+
+        [SetUp]
+        public void TestSetup()
+        {
+            this.OnTestSetup();
+        }
+
+        protected virtual void OnTestFixtureSetUp()
+        {
+        }
+
+        protected virtual void OnTestSetup()
+        {
+        }
+    }
+}
---
+++ test/ChamaeleoIoT.Tests/Framework/TestEnvironment{TSubject}.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ChamaeleoIoT.Tests
+{
+    public abstract class TestEnvironment<TSubject>
+    {
+        private TSubject _subject;
+
+        public TSubject Subject
+        {
+            get
+            {
+                if (_subject == null)
+                {
+                    _subject = this.CreateSubjectInstance();
+                }
+
+                return _subject;
+            }
+        }
+
+        public void SetupEnvironment()
+        {
+            this.OnSetupEnvironment();
+        }
+
+        protected virtual TSubject CreateSubjectInstance()
+        {
+            return Activator.CreateInstance<TSubject>();
+        }
+
+        protected abstract void OnSetupEnvironment();
+    }
+}
