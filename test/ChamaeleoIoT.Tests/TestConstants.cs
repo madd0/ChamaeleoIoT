aedb151c9bf9b9d61ff712008d286a566239a3d9
Mauricio Díaz Orlich <github@madd0.com>
2016-11-06 13:46:40 +0100

    Add extension to NodaTime.Instant to return Julian date
    The method exists in the NodaTime master branch, but not in the current NuGet package. When NodaTime
    updates its Nuget package to v2.0, this can be removed.

---
+++ test/ChamaeleoIoT.Tests/TestConstants.cs
@@ -0,0 +1,32 @@
+﻿using NodaTime;
+
+namespace ChamaeleoIoT.Tests
+{
+    public static class TestConstants
+    {
+        public const double JulianNow = 2456540.05156;
+
+        static TestConstants()
+        {
+            Today = new ZonedDateTime(Now, TestTimeZone);
+
+            SunriseToday = new ZonedDateTime(SunriseInstant, TestTimeZone);
+
+            SunsetToday = new ZonedDateTime(SunsetInstant, TestTimeZone);
+        }
+
+        public static Instant Now { get; } = Instant.FromUtc(2013, 9, 4, 13, 14, 15);
+
+        public static Instant SunriseInstant { get; } = Instant.FromUtc(2013, 9, 4, 7, 13, 0);
+
+        public static ZonedDateTime SunriseToday { get; }
+
+        public static Instant SunsetInstant { get; } = Instant.FromUtc(2013, 9, 4, 20, 28, 0);
+
+        public static ZonedDateTime SunsetToday { get; }
+
+        public static DateTimeZone TestTimeZone { get; } = DateTimeZoneProviders.Tzdb.GetZoneOrNull("Europe/Paris");
+
+        public static ZonedDateTime Today { get; }
+    }
+}
