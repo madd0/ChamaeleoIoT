0bdaf6d95a92446fbe14833c770393c4e6dd40a1
Mauricio Díaz Orlich <github@madd0.com>
2016-11-06 22:23:48 +0100

    File cleanup

--- src/ChamaeleoIoT.Service/StartupTask.cs
+++ src/ChamaeleoIoT.Service/StartupTask.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
-using Windows.ApplicationModel.Background;
+﻿using Windows.ApplicationModel.Background;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -13,13 +8,10 @@ namespace ChamaeleoIoT.Service
     {
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
             // TODO: Insert code to perform background work
             //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
+            // If you start any asynchronous methods here, prevent the task from closing prematurely
+            // by using BackgroundTaskDeferral as described in http://aka.ms/backgroundtaskdeferral
         }
     }
 }
